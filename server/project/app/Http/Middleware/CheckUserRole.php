4e75e5a7f74e83037c364295f190a86dc79e5715
Haruaki <32154385+haruaki07@users.noreply.github.com>
2024-08-30 02:18:02 +0000

    fix(server): check role middleware

--- server/project/app/Http/Middleware/CheckUserRole.php
+++ server/project/app/Http/Middleware/CheckUserRole.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Enum\UserRole;
+use App\Exceptions\InvariantException;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,8 +19,8 @@ class CheckUserRole
     {
         $user = $request->user();
 
-        if (! $user || !in_array($user->role, $roles)) {
-            new InvariantException('unauthorized', 403);
+        if (!$user || !in_array($user->role->value, $roles)) {
+            throw new InvariantException('unauthorized', 403);
         }
 
         return $next($request);
